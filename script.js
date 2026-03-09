b9f19c52664faa51a131ed78ce3f3f2dc0cc91ae
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-09 19:07:53 +0530

    Add javascript selectors

---
+++ script.js
@@ -0,0 +1,8 @@
+const input  = document.querySelector("input");
+const button = document.querySelector("button");
+
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const repos = document.getElementById("repos");
+
+const userStats = document.getElementById("userStats");
